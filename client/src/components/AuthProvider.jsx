f1bcfdba30ffccae5a04208dd358f7ca26761448
Kaxnovskiy_Eugene <Kaxxa2927@mail.ru>
2023-12-07 00:03:46 +0300

    Added a reboot page check in AuthProvider.

--- client/src/components/AuthProvider.jsx
+++ client/src/components/AuthProvider.jsx
@@ -1,9 +1,16 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import AuthContext from '../context/authcontext'
 
 const AuthProvider = ({ children }) => {
     const [isAuth, setIsAuth] = useState(false);
-  
+
+    useEffect(() => {
+      const token = localStorage.getItem('token');
+      if (token) {
+        setIsAuth(true);
+      }
+    }, []);
+
     return (
       <AuthContext.Provider value={{ isAuth, setIsAuth }}>
         {children}
