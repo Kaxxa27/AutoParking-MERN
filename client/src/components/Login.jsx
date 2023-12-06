9dcb932cb5dde41c45d8097697282f1fc59dd18e
Kaxnovskiy_Eugene <Kaxxa2927@mail.ru>
2023-12-06 21:38:52 +0300

    Small fixed in Login component.

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,37 +1,37 @@
-// Login.js
 import React, { useState } from 'react';
+import MainButton from './UI/button/MainButton';
 import $api from '../http/index';
 
 const Login = ({ onLogin }) => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+   const [email, setEmail] = useState('');
+   const [password, setPassword] = useState('');
 
-  const handleLogin = async () => {
-    try {
-      const response = await $api.post('/auth/login', { email, password });
-      const { token } = response.data;
+   const handleLogin = async (e) => {
+      try {
+         const response = await $api.post('/auth/login', { email, password });
+         const { token } = response.data;
 
-      localStorage.setItem('token', token);
-      onLogin();
-    } catch (error) {
-      console.error('Error during login:', error);
-    }
-  };
+         localStorage.setItem('token', token);
+         onLogin();
+      } catch (error) {
+         console.error('Error during login:', error);
+      }
+   };
 
-  return (
-    <div>
-      <h2>Login</h2>
-      <form>
-        <label>Email:</label>
-        <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
-        <br />
-        <label>Password:</label>
-        <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-        <br />
-        <button type="button" onClick={handleLogin}>Login</button>
-      </form>
-    </div>
-  );
+   return (
+      <div>
+         <h2>Login</h2>
+         <form>
+            <label>Email:</label>
+            <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+            <br />
+            <label>Password:</label>
+            <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+            <br />
+            <button onClick={handleLogin}>Login</button>
+         </form>
+      </div>
+   );
 };
 
 export default Login;
