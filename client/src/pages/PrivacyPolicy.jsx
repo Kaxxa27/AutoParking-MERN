37dfd8a0cd6567bbfe25742ae5850e1ba0b74c5b
Kaxnovskiy_Eugene <Kaxxa2927@mail.ru>
2023-12-08 02:13:50 +0300

    Added current date.

--- client/src/pages/PrivacyPolicy.jsx
+++ client/src/pages/PrivacyPolicy.jsx
@@ -1,9 +1,42 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 const PrivacyPolicy = () => {
+  const [userTimeZone, setUserTimeZone] = useState('Europe/Minsk'); // Replace with the actual user time zone
+  const [dataModifiedDate, setDataModifiedDate] = useState('');
+  const [error, setError] = useState('');
+
+  useEffect(() => {
+    getDataModifiedDate();
+  }, [userTimeZone]);
+
+  const getDataModifiedDate = () => {
+    try {
+      // Replace this with the actual logic to fetch data modification date
+      // For demonstration, using the current date as data modification date
+      const now = new Date();
+      if (!isNaN(now.getTime())) {
+        const formattedDate = new Intl.DateTimeFormat('en-US', {
+          timeZone: userTimeZone,
+          dateStyle: 'full',
+          timeStyle: 'long',
+        }).format(now);
+        setDataModifiedDate(formattedDate);
+      } else {
+        throw new Error('Invalid date');
+      }
+    } catch (error) {
+      setError(`Error: ${error.message}`);
+    }
+  };
+
   return (
     <div>
-      <h1>Политика Конфиденциальности</h1>
+      {dataModifiedDate && (
+        <p>
+          {`Data Modification Date (Europe/Minsk): ${dataModifiedDate}`}
+        </p>
+      )}
+      {error && <p>{error}</p>}
     </div>
   );
 };
