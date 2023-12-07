f707fc40e59b51bb48f25dc81c4181f5fcb6a936
Kaxnovskiy_Eugene <Kaxxa2927@mail.ru>
2023-12-07 21:26:14 +0300

    Added table folder in UI.

---
+++ client/src/components/UI/Tabel/ParkingTable/ParkingTable.jsx
@@ -0,0 +1,66 @@
+import React, { useContext, useState } from 'react';
+import AuthContext from '../../../../context/authcontext';
+import CreateParkingForm from '../../../CreateParkingForm';
+import $api from '../../../../http/index';
+
+const ParkingTable = ({ parkings }) => {
+  const { isAuth } = useContext(AuthContext);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [updatedParking, setUpdatedParking] = useState(null);
+
+  const handleEdit = (parking) => {
+    setUpdatedParking(parking);
+    setIsModalOpen(true);
+  };
+
+  const handleDelete = async (parkingId) => {
+    try {
+      await $api.delete(`/parkingspots/${parkingId}`);
+      // Обновляем данные после удаления
+      // Можете использовать ваш механизм для загрузки данных снова
+      // Например, вызвать функцию, которая загружает данные из API и обновляет состояние
+    } catch (error) {
+      console.error('Ошибка при удалении парковки:', error.response.data.message);
+    }
+  };
+
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Номер</th>
+          <th>Цена</th>
+          <th>Статус</th>
+          <th>Владелец</th>
+          <th>Машины</th>
+          <th>Дата аренды</th>
+          {isAuth && <th>Действия</th>}
+        </tr>
+      </thead>
+      <tbody>
+        {parkings.map((parking) => (
+          <tr key={parking.id}>
+            <td>{parking.number}</td>
+            <td>{parking.price}</td>
+            <td>{parking.is_busy ? 'Занято' : 'Свободно'}</td>
+            <td>{parking.user ? parking.user : 'Отсутствует'}</td>
+            <td>{parking.cars.count ? parking.cars : 'Отсутствуют'}</td>
+            <td>{parking.date_of_rent ? parking.date_of_rent : ' Не Арендована'}</td>
+            {isAuth && (
+              <td>
+                <button onClick={() => handleEdit(parking)}>Изменить</button>
+                <button onClick={() => handleDelete(parking.id)}>Удалить</button>
+              </td>
+            )}
+          </tr>
+        ))}
+      </tbody>
+      
+        {isModalOpen && (
+          <CreateParkingForm visible={isModalOpen} setVisible={setIsModalOpen} isCreate = {false}  updatedParking={updatedParking} />
+        )}  
+    </table>
+  );
+};
+
+export default ParkingTable;
