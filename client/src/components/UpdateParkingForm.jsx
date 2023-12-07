92991772c4d83f90fc63346be7a24933a408a3ba
Kaxnovskiy_Eugene <Kaxxa2927@mail.ru>
2023-12-07 22:35:02 +0300

    Added crud for Parking

--- client/src/components/UpdateParkingForm.jsx
+++ client/src/components/UpdateParkingForm.jsx
@@ -1,15 +1,23 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import MyModal from './UI/MyModal/MyModal';
 import $api from '../http/index'
 import { useNavigate } from 'react-router-dom';
 
-const CreateParkingForm = ({ visible, setVisible, loadParkingSpots }) => {
+const UpdateParkingForm = ({ parkingSpot, visible, setVisible, loadParkingSpots }) => {
 
     const navigate = useNavigate();
     const [formData, setFormData] = useState({
         number: 0,
         price: 0
     });
+
+    useEffect(() => {
+        if (parkingSpot) {
+            setFormData({
+              ...parkingSpot
+            });
+        }
+    }, []);
 
     const handleChange = (e) => {
         const { name, value } = e.target;
@@ -23,7 +31,8 @@ const CreateParkingForm = ({ visible, setVisible, loadParkingSpots }) => {
         e.preventDefault();
 
         try {
-            await $api.post('/parkingspots', formData);
+
+            await $api.put(`/parkingspots/${parkingSpot._id}`, formData);
             loadParkingSpots();
             navigate('/parking-catalog');
         } catch (error) {
@@ -35,21 +44,21 @@ const CreateParkingForm = ({ visible, setVisible, loadParkingSpots }) => {
 
     return (
         <MyModal visible={visible} setVisible={setVisible}>
-            <h2>Создать парковку</h2>
+            <h2>Обновить парковку</h2>
             <form onSubmit={handleSubmit}>
 
-                <label>Номер:</label>    
+                <label>Номер:</label>
                 <input type="number" name="number" value={formData.number} onChange={handleChange} />
 
                 <label>Цена:</label>
                 <input type="number" name="price" value={formData.price} onChange={handleChange} required />
 
-                <button type="submit">Создать</button>
-                <button onClick={() => setVisible(false)}>Отмена</button>
+                <button type="submit">Обновить</button>
+                <button type='reset' onClick={() => setVisible(false)}>Отмена</button>
             </form>
-          
+
         </MyModal>
     );
 };
 
-export default CreateParkingForm;
+export default UpdateParkingForm;
